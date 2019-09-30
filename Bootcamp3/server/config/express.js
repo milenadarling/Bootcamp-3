96f9c972acc3132d0c57a9619dc94531088f6aef
Milena <darlingm@ufl.edu>
2019-09-30 14:08:20 -0400

    add

--- Bootcamp3/server/config/express.js
+++ Bootcamp3/server/config/express.js
@@ -46,13 +46,15 @@ module.exports.init = function() {
      Sends a response (res) to go to the homepage for all routes not specified */ 
   app.all('/*', function(req, res) {
    
+    app.use(express.static('public'))
    /*Add YOUR CODE HERE 
       see https://expressjs.com/en/api.html#res.sendFile
       see https://nodejs.org/api/path.html
       The path.resolve() method returns a string and resolves a sequence of paths or path segments into an absolute path.
       If no path segments are passed, path.resolve() will return the absolute path of the current working directory.
    */
-   //res.sendFile(path.resolve(...));
+
+   res.sendFile(path.resolve('client/index.html'));
   });
   
   return app;
